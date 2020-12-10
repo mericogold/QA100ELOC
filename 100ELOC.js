68024e071cd0e46f4920c556bd0b7490c9b1a119
mericogold <github4merico+gold@s.gonze.com>
2020-12-10 15:54:31 -0800

    With luck this will be 50

---
+++ 100ELOC.js
@@ -0,0 +1,13 @@
+const attempt = (fn, ...args) => {
+  try {
+    return fn(...args);
+  } catch (e) {
+    return e instanceof Error ? e : new Error(e);
+  }
+};
+var elements = attempt(function(selector) {
+  return document.querySelectorAll(selector);
+}, '>_>');
+if (elements instanceof Error) elements = []; // elements = []
+
+alert("nop");
