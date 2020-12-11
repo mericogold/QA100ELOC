6e15c30e71ec59c827c091458c739a0ca564812a
mericogold <github4merico+gold@s.gonze.com>
2020-12-10 16:01:33 -0800

    Trying again for 100

--- 100ELOC.js
+++ 100ELOC.js
@@ -1,13 +1,25 @@
-const attempt = (fn, ...args) => {
-  try {
-    return fn(...args);
-  } catch (e) {
-    return e instanceof Error ? e : new Error(e);
-  }
-};
-var elements = attempt(function(selector) {
-  return document.querySelectorAll(selector);
-}, '>_>');
-if (elements instanceof Error) elements = []; // elements = []
+/* @flow */
 
-alert("nop");
+import { install } from './install'
+import { START } from './util/route'
+import { assert } from './util/warn'
+import { inBrowser } from './util/dom'
+import { cleanPath } from './util/path'
+import { createMatcher } from './create-matcher'
+import { normalizeLocation } from './util/location'
+import { supportsPushState } from './util/push-state'
+import { handleScroll } from './util/scroll'
+
+import { HashHistory } from './history/hash'
+import { HTML5History } from './history/html5'
+import { AbstractHistory } from './history/abstract'
+
+import type { Matcher } from './create-matcher'
+
+import { isNavigationFailure, NavigationFailureType } from './util/errors'
+
+export default class VueRouter {
+  static install: () => void
+  static version: string
+  static isNavigationFailure: Function
+  static NavigationFailureType: any
